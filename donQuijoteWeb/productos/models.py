4b1ff5ba671711271b920c081b7ecd3167324d90
RAArrieta <alfredoarrieta99@gmail.com>
2024-06-14 17:55:09 -0300

    models productos

--- donQuijoteWeb/productos/models.py
+++ donQuijoteWeb/productos/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+class ProductoCategoria(models.Model):
+    nombre=models.CharField(max_length=50)
+    imagen=models.ImageField(blank=True, null=True)
+    
+    def __str__(self):
+        return self.nombre
+    
+class Producto(models.Model):
+    nombre=models.CharField(max_length=50, verbose_name="Nobre")
+    categoria=models.ForeignKey(ProductoCategoria, on_delete=models.CASCADE, verbose_name="Categoría")
+    descripcion=models.CharField(max_length=200, blank=True, null=True, verbose_name="Descripción")
+    precio_unit=models.IntegerField(verbose_name="P. Unitario")
+    precio_media=models.IntegerField(blank=True, null=True, verbose_name="P. Media")
+    precio_doc=models.IntegerField(blank=True, null=True, verbose_name="P. Docena")
+    
+    def __str__(self):
+        return self.nombre
