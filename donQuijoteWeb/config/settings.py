185234fe20f689a37c9eb9dccf7edc5f07dc8a06
RAArrieta <alfredoarrieta99@gmail.com>
2024-06-14 18:16:24 -0300

    config static img

--- donQuijoteWeb/config/settings.py
+++ donQuijoteWeb/config/settings.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -104,6 +105,14 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
+
+# BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+
+
 
 STATIC_URL = '/static/'
 
--- donQuijoteWeb/productos/models.py
+++ donQuijoteWeb/productos/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class ProductoCategoria(models.Model):
     nombre=models.CharField(max_length=50)
-    imagen=models.ImageField(blank=True, null=True)
+    imagen=models.ImageField(upload_to='productos/img/',blank=True, null=True)
     
     def __str__(self):
         return self.nombre
