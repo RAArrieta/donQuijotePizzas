4b96ba9c8fe9a791e237a0ffe0df83fd547f5331
RAArrieta <alfredoarrieta99@gmail.com>
2024-06-24 18:13:10 -0300

    .

--- donQuijoteWeb/carro/funciones.py
+++ donQuijoteWeb/carro/funciones.py
@@ -12,52 +12,62 @@ def select_productos():
 
 class Carro:
     def __init__(self, request):
-        self.request=request
-        self.session=request.session
-        carro=self.session.get("carro")
+        self.request = request
+        self.session = request.session
+        carro = self.session.get("carro")
         if not carro:
-            carro=self.session["carro"]={}
-        self.carro=carro
+            carro = self.session["carro"] = {}
+        self.carro = carro
 
     def agregar(self, producto):
-        if (str(producto.id) not in self.carro.keys()):
-            self.carro[producto.id]={
-                "producto_id":producto.id,
-                "nombre":producto.nombre,
-                "precio_unit":str(producto.precio_unit),
-                "precio_media":str(producto.precio_media),
-                "precio_doc":str(producto.precio_doc),
-                "cantidad":1,
-                "categoria":str(producto.categoria)
+        producto_id_str = str(producto.id)
+        if producto_id_str not in self.carro.keys():
+            self.carro[producto.id] = {
+                "producto_id": producto.id,
+                "nombre": producto.nombre,
+                "precio_unit": str(producto.precio_unit),
+                "precio_media": str(producto.precio_media),
+                "precio_doc": str(producto.precio_doc),
+                "cantidad": 1,
+                "subtotal": float(producto.precio_unit),  # Inicializar con el precio unitario del producto
+                "categoria": str(producto.categoria)
             }
         else:
             for key, value in self.carro.items():
-                if key==str(producto.id):
-                    value["cantidad"]=value["cantidad"]+1
-                    value["precio_unit"]=float(value["precio_unit"])+producto.precio_unit
+                if key == producto_id_str:
+                    value["cantidad"] += 1
+                    value["subtotal"] = self.calcular_precio(producto, value["subtotal"])
                     break
         self.guardar_carro()
 
     def guardar_carro(self):
-        self.session["carro"]=self.carro
-        self.session.modified=True
+        self.session["carro"] = self.carro
+        self.session.modified = True
 
     def eliminar(self, producto):
-        producto.id=str(producto.id)
-        if producto.id in self.carro:
-            del self.carro[producto.id]
+        producto_id_str = str(producto.id)
+        if producto_id_str in self.carro:
+            del self.carro[producto_id_str]
             self.guardar_carro()
 
     def restar_producto(self, producto):
+        producto_id_str = str(producto.id)
         for key, value in self.carro.items():
-            if key==str(producto.id):
-                value["cantidad"]=value["cantidad"]-1
-                value["precio_unit"]=float(value["precio_unit"])-producto.precio_unit
-                if value["cantidad"]<1:
+            if key == producto_id_str:
+                value["cantidad"] -= 1
+                value["subtotal"] = self.calcular_precio(producto, value["subtotal"], subtract=True)
+                if value["cantidad"] < 1:
                     self.eliminar(producto)
                 break
         self.guardar_carro()
 
     def limpiar_carro(self):
-        self.session["carro"]={}
-        self.session.modified=True
+        self.session["carro"] = {}
+        self.session.modified = True
+
+    def calcular_precio(self, producto, subtotal, subtract=False):
+        subtotal = float(subtotal)
+        if subtract:
+            return subtotal - producto.precio_unit
+        else:
+            return subtotal + producto.precio_unit
