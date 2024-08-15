7d73df2ec826cb42c7eb1e9f68f934ec12d2ae9e
RAArrieta <alfredoarrieta99@gmail.com>
2024-08-15 17:58:30 -0300

    factura diaria

--- donQuijoteWeb/core/context_processors.py
+++ donQuijoteWeb/core/context_processors.py
@@ -4,5 +4,6 @@ def counter_pedidos(request):
     cant_pedidos = Pedido.objects.all().count()
     cant_entregados = Pedido.objects.filter(estado='entregado').count()
     cant_pendientes = Pedido.objects.filter(estado='pendiente').count()
+    cant_reservados = Pedido.objects.filter(estado='reservado').count()
     
-    return {'cant_pedidos':cant_pedidos, "cant_entregados":cant_entregados, "cant_pendientes":cant_pendientes }
+    return {'cant_pedidos':cant_pedidos, "cant_entregados":cant_entregados, "cant_pendientes":cant_pendientes, "cant_reservados":cant_reservados }
