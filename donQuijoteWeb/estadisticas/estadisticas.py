a6a47361b291ac952c1d9b24cec3e67ee333495f
RAArrieta <alfredoarrieta99@gmail.com>
2024-09-25 18:14:40 -0300

    estadisticas

--- donQuijoteWeb/estadisticas/estadisticas.py
+++ donQuijoteWeb/estadisticas/estadisticas.py
@@ -1,130 +1,183 @@
-from datetime import timedelta
+import calendar
+from datetime import timedelta, date
+from django.utils import timezone
 from facturas.models import FacturaProducto
 from productos.models import Producto, ProductoCategoria
 from django.db.models import Sum, Min, Max, Avg, Count
 
 class Estadisticas:
-    def __init__(self, producto_id,  producto_nombre, categoria, cantidad_vendida, fecha_inicio, fecha_fin, dia_semana, cantidad_dias):
+    def __init__(self, producto_id,  producto_nombre, categoria, cantidad_vendida, cantidad_promedio, fecha_inicio, fecha_fin, dia_semana, media_semana, mes, ano, cantidad_dias):
         self.producto_id = producto_id
         self.producto_nombre = producto_nombre
         self.categoria= categoria
         self.cantidad_vendida = cantidad_vendida
-        # self.cantidad_promedio = cantidad_promedio
+        self.cantidad_promedio = cantidad_promedio
         # self.cantidad_minima = cantidad_minima
         # self.cantidad_maxima = cantidad_maxima
         # self.cantidad_no_vendida = cantidad_no_vendida
         self.fecha_inicio = fecha_inicio
         self.fecha_fin = fecha_fin
         self.dia_semana = dia_semana
+        self.media_semana = media_semana
+        self.mes = mes
+        self.ano = ano
         self.cantidad_dias = cantidad_dias
         
     def calcular_cantidad_vendida(self):
-        if self.producto_id:
             try:
-                producto_seleccionado = Producto.objects.get(id=self.producto_id)
-                productos_vendidos = FacturaProducto.objects.filter(
-                    producto=producto_seleccionado
-                )
-                if self.fecha_inicio and self.fecha_fin:
-                    productos_vendidos = productos_vendidos.filter(
-                        factura__fecha__range=[self.fecha_inicio, self.fecha_fin]
-                    )
-                if self.dia_semana:
-                    productos_vendidos = productos_vendidos.filter(
-                        factura__fecha__week_day=int(self.dia_semana)
-                    )
+                productos_vendidos = Estadisticas.productos_vendidos(self)
                 self.cantidad_vendida = productos_vendidos.aggregate(total_vendido=Sum('cantidad'))['total_vendido'] or 0  
+                Estadisticas.calcular_cantidad_dias(self, productos_vendidos)
+                if self.cantidad_dias > 0 and self.cantidad_vendida > 0:
+                    self.cantidad_promedio = self.cantidad_vendida / self.cantidad_dias
+                else:
+                    self.cantidad_promedio = 0
             except Producto.DoesNotExist:
                 print(f"Producto {self.producto_nombre} no existe.")
                 
-        elif self.categoria:
-            try:
-                productos_categoria = Producto.objects.filter(categoria__nombre=self.categoria)
-                productos_vendidos = FacturaProducto.objects.filter(
-                    producto__in=productos_categoria
+    def productos_vendidos(self):
+        if self.producto_id:
+            producto_seleccionado = Producto.objects.get(id=self.producto_id) 
+            fecha_actual = timezone.now()
+            fecha_inicio = fecha_actual - timedelta(days=85)
+            productos_vendidos = FacturaProducto.objects.filter(
+                producto=producto_seleccionado,
+                factura__fecha__range=(fecha_inicio, fecha_actual)
+            )
+            if self.fecha_inicio and self.fecha_fin:
+                productos_vendidos = productos_vendidos.filter(
+                    factura__fecha__range=[self.fecha_inicio, self.fecha_fin]
                 )
-                if self.fecha_inicio and self.fecha_fin:
+            if self.dia_semana:
+                productos_vendidos = productos_vendidos.filter(
+                    factura__fecha__week_day=int(self.dia_semana)
+                )  
+            if self.media_semana:
+                if self.media_semana == "m_j":
                     productos_vendidos = productos_vendidos.filter(
-                        factura__fecha__range=[self.fecha_inicio, self.fecha_fin]
-                    )
-                if self.dia_semana:
-                    productos_vendidos = productos_vendidos.filter(
-                        factura__fecha__week_day=int(self.dia_semana)
-                    )
-                self.cantidad_vendida = productos_vendidos.aggregate(total_vendido=Sum('cantidad'))['total_vendido'] or 0
-            except ProductoCategoria.DoesNotExist:
-                print(f"Categoría {self.categoria} no existe.")
-                
-                
-    def calcular_cantidad_dias(self):
-        if self.fecha_inicio and self.fecha_fin:
-            self.cantidad_dias = (self.fecha_fin - self.fecha_inicio).days + 1
-        elif self.dia_semana:
-            try:
-                if self.producto_id:
-                    producto_seleccionado = Producto.objects.get(id=self.producto_id)
-                    productos_vendidos = FacturaProducto.objects.filter(
-                        producto=producto_seleccionado,
-                        factura__fecha__week_day=int(self.dia_semana)
-                    )
-                elif self.categoria:
-                    productos_categoria = Producto.objects.filter(categoria__nombre=self.categoria)
-                    productos_vendidos = FacturaProducto.objects.filter(
-                        producto__in=productos_categoria,
-                        factura__fecha__week_day=int(self.dia_semana)
+                        factura__fecha__week_day__in=[3, 4, 5]
                     )
                 else:
-                    productos_vendidos = FacturaProducto.objects.none()
-
+                    productos_vendidos = productos_vendidos.filter(
+                        factura__fecha__week_day__in=[6, 7, 1]
+                    )
+            if self.mes and self.ano:
+                if self.mes != "0":
+                    mes = int(self.mes)
+                    ano = int(self.ano)
+                    productos_vendidos = productos_vendidos.filter(
+                        factura__fecha__month=mes,
+                        factura__fecha__year=ano
+                    ) 
+                else:
+                    ano = int(self.ano)
+                    productos_vendidos = productos_vendidos.filter(factura__fecha__year=ano) 
+                    
+        elif self.categoria:
+            productos_categoria = Producto.objects.filter(categoria__nombre=self.categoria)
+            fecha_actual = timezone.now()
+            fecha_inicio = fecha_actual - timedelta(days=85)
+            productos_vendidos = FacturaProducto.objects.filter(
+                producto__in=productos_categoria,
+                factura__fecha__range=(fecha_inicio, fecha_actual)
+            )
+            if self.fecha_inicio and self.fecha_fin:
+                productos_vendidos = productos_vendidos.filter(
+                    factura__fecha__range=[self.fecha_inicio, self.fecha_fin]
+                )
+            if self.dia_semana:
+                productos_vendidos = productos_vendidos.filter(
+                    factura__fecha__week_day=int(self.dia_semana)
+                )  
+            if self.media_semana:
+                if self.media_semana == "m_j":
+                    productos_vendidos = productos_vendidos.filter(
+                        factura__fecha__week_day__in=[3, 4, 5]
+                    )
+                else:
+                    productos_vendidos = productos_vendidos.filter(
+                        factura__fecha__week_day__in=[6, 7, 1]
+                    )
+            if self.mes and self.ano:
+                if self.mes != "0":
+                    mes = int(self.mes)
+                    ano = int(self.ano)
+                    productos_vendidos = productos_vendidos.filter(
+                        factura__fecha__month=mes,
+                        factura__fecha__year=ano
+                    ) 
+                else:
+                    ano = int(self.ano)
+                    productos_vendidos = productos_vendidos.filter(
+                        factura__fecha__year=ano
+                    )               
+        return productos_vendidos
+               
+    def calcular_cantidad_dias(self, productos_vendidos):
+        if self.fecha_inicio and self.fecha_fin:
+            self.cantidad_dias = (self.fecha_fin - self.fecha_inicio).days + 1           
+        elif self.dia_semana:
+            try:
                 if productos_vendidos.exists():
-                    # Obtener la fecha más antigua y más reciente de ventas
                     primera_fecha = productos_vendidos.earliest('factura__fecha').factura.fecha
-                    ultima_fecha = productos_vendidos.latest('factura__fecha').factura.fecha
-
-                    dia_semana_deseado = int(self.dia_semana)
-                    cantidad_dias_semana = 1
-
+                    ultima_fecha = date.today()
+                    dia_semana_deseado = int(self.dia_semana) - 2
+                    cantidad_dias_semana = 0
                     fecha_actual = primera_fecha
+                    
                     while fecha_actual <= ultima_fecha:
                         if fecha_actual.weekday() == dia_semana_deseado:  
                             cantidad_dias_semana += 1
                         fecha_actual += timedelta(days=1)  
                         
-                    self.cantidad_dias = cantidad_dias_semana
-
-                # if productos_vendidos.exists():
-                #     # Obtener la fecha más antigua y más reciente de ventas
-                #     primera_fecha = productos_vendidos.earliest('factura__fecha').factura.fecha
-                #     ultima_fecha = productos_vendidos.latest('factura__fecha').factura.fecha
-                #     print(f"Primera fecha: {primera_fecha}, Ultima fecha: {ultima_fecha}")
-                    
+                    self.cantidad_dias = cantidad_dias_semana                    
                 else:
                     self.cantidad_dias = 0
             except Producto.DoesNotExist:
                 print(f"Producto {self.producto_nombre} no existe.")
             except ProductoCategoria.DoesNotExist:
                 print(f"Categoría {self.categoria} no existe.")
+        elif self.media_semana:
+            try:
+                if productos_vendidos.exists():
+                    primera_fecha = productos_vendidos.earliest('factura__fecha').factura.fecha
+                    ultima_fecha = date.today()
+                    cantidad_dias_semana = 0
+                    fecha_actual = primera_fecha
+                    
+                    if self.media_semana == "m_j":
+                        while fecha_actual <= ultima_fecha:
+                            if fecha_actual.weekday() in [1, 2, 3]: 
+                                cantidad_dias_semana += 1
+                            fecha_actual += timedelta(days=1)
+                            
+                        self.cantidad_dias = cantidad_dias_semana
+                    else:
+                        while fecha_actual <= ultima_fecha:
+                            if fecha_actual.weekday() in [4, 5, 6]:  
+                                cantidad_dias_semana += 1
+                            fecha_actual += timedelta(days=1)
+                            
+                        self.cantidad_dias = cantidad_dias_semana
+                else:
+                    self.cantidad_dias = 0
+            except Producto.DoesNotExist:
+                print(f"Producto {self.producto_nombre} no existe.")
+            except ProductoCategoria.DoesNotExist:
+                print(f"Categoría {self.categoria} no existe.")
+        elif self.mes and self.ano:
+            if self.mes != "0":
+                mes = int(self.mes)
+                ano = int(self.ano)
+                print(f"mes: {mes}")
+                _, self.cantidad_dias = calendar.monthrange(ano, mes)
+            else:
+                self.cantidad_dias = 365
+                
         else:
             self.cantidad_dias = 0
 
-                
-    # def calcular_cantidad_dias(self):
-    #     if self.fecha_inicio and self.fecha_fin:
-    #         self.cantidad_dias = (self.fecha_fin - self.fecha_inicio).days + 1
-    #     elif self.dia_semana:
-    #         self.cantidad_dias = 24
-            
-    #         productos_vendidos = productos_vendidos.filter(factura__fecha__week_day=int(self.dia_semana))
-    #         print(productos_vendidos)
-    #         # Obtener la fecha más antigua y más reciente de ventas los días miércoles.
-    #         primera_fecha = productos_vendidos.earliest('factura__fecha').factura.fecha
-    #         ultima_fecha = productos_vendidos.latest('factura__fecha').factura.fecha
-    #         print(f"Primera fecha: {primera_fecha}, Ultima fecha: {ultima_fecha}")
-
-
-    #     else:
-    #         self.cantidad_dias = 0
-                    
+                   
     def calcular_minimo():
         pass
 
--- donQuijoteWeb/estadisticas/views.py
+++ donQuijoteWeb/estadisticas/views.py
@@ -4,20 +4,24 @@ from django.utils.dateparse import parse_date
 from carro.select_productos import select_productos
 from .estadisticas import Estadisticas
 from productos.models import Producto
+from .forms import MesAnoForm
 
 def home(request):
     categorias = select_productos()
     estadisticas = request.session.get('estadisticas', None)
-    
+    form = MesAnoForm(request.POST or None)
+  
     context = {
         'categorias': categorias,
         'estadisticas': estadisticas,  
+        'form': form,
     }
     
     return render(request, "estadisticas/index.html", context)
 
 def cargar_datos(request):
-    if request.method == 'POST':              
+    if request.method == 'POST':    
+        categoria = request.POST.get('categoria', None)          
         producto_id = request.POST.get('producto_id', None)
         if producto_id:
             try:
@@ -26,142 +30,49 @@ def cargar_datos(request):
                 producto_nombre = None
         else:
             producto_nombre = None
-        categoria = request.POST.get('categoria', None)
+        
         fecha_inicio = request.POST.get("fecha_inicio", None)
         fecha_fin = request.POST.get("fecha_fin", None)
-        dia_semana = request.POST.get("dia_semana", None)
-        
         fecha_inicio = parse_date(fecha_inicio) if fecha_inicio else None
         fecha_fin = parse_date(fecha_fin) if fecha_fin else None
-        
-        if (producto_id or categoria) and (fecha_inicio or dia_semana):
+        dia_semana = request.POST.get("dia_semana", None)
+        media_semana = request.POST.get("media_semana", None)
+        mes = request.POST.get("mes", None)
+        ano = request.POST.get("ano", None)
+              
+        if producto_id or categoria:
             estadisticas = Estadisticas(
                 producto_id=producto_id,
                 producto_nombre=producto_nombre,
                 categoria=categoria,
                 cantidad_vendida=0, 
+                cantidad_promedio=0,
                 fecha_inicio=fecha_inicio,
                 fecha_fin=fecha_fin,
                 dia_semana=dia_semana,
+                media_semana=media_semana,
+                mes=mes if (fecha_inicio is None and dia_semana is None and media_semana is None) else None,
+                ano=ano if (fecha_inicio is None and dia_semana is None and media_semana is None) else None,
                 cantidad_dias=0
             )
-
-            estadisticas.calcular_cantidad_vendida()
-            estadisticas.calcular_cantidad_dias()
+            
+            estadisticas.calcular_cantidad_vendida() 
 
             request.session['estadisticas'] = {
                 'producto_id': producto_id,
                 'producto_nombre': producto_nombre,
                 'categoria': categoria,
                 'cantidad_vendida': estadisticas.cantidad_vendida, 
+                'cantidad_promedio': estadisticas.cantidad_promedio,
                 'fecha_inicio': str(fecha_inicio) if fecha_inicio else None,
                 'fecha_fin': str(fecha_fin) if fecha_fin else None,
                 'dia_semana': dia_semana,
+                'media_semana': media_semana,
+                'mes': mes if (fecha_inicio is None and dia_semana is None and mes and ano) else None,
+                'ano': ano if (fecha_inicio is None and dia_semana is None and mes and ano) else None,
                 'cantidad_dias': estadisticas.cantidad_dias,     
             }
         else:
-            messages.warning(request, "Debe seleccionar un producto o categoria y filtrar una fecha...")
+            messages.warning(request, "Debe seleccionar un producto o categoría y filtrar una fecha...")
     
     return redirect('estadisticas:home')
-
-# def home(request):
-#     categorias = select_productos()
-    
-#     estadisticas = Estadisticas()
-    
-#     print(f"Producto Seleccionado: {estadisticas.producto_seleccionado}")
-#     print(f"Categoría Seleccionada: {estadisticas.categoria_seleccionada}")
-#     print(f"Fecha inicio: {estadisticas.fecha_inicio}")
-#     print(f"Fecha Fin: {estadisticas.fecha_fin}")
-#     print(f"Dia Seleccionado: {estadisticas.dia_semana}")
-#     estadisticas = Estadisticas()
-#     productos_vendidos = FacturaProducto.objects.all()
-
-#     producto_seleccionado = None
-#     categoria_seleccionada = None
-#     cantidad_vendida = 0
-#     cantidad_promedio = 0
-#     cantidad_minima = 0
-#     cantidad_maxima = 0
-#     cantidad_no_vendida = 0
-
-
-
-#         if producto_id:
-#             try:
-#                 producto_seleccionado = Producto.objects.get(id=producto_id)
-                
-#                 productos_vendidos = FacturaProducto.objects.filter(
-#                     producto=producto_seleccionado
-#                 )
-#                 if fecha_inicio and fecha_fin:
-#                     productos_vendidos = productos_vendidos.filter(
-#                         factura__fecha__range=[fecha_inicio, fecha_fin]
-#                     )
-#                 if dia_semana:
-#                     productos_vendidos = productos_vendidos.filter(
-#                         factura__fecha__week_day=int(dia_semana)
-#                     )
-#                 cantidad_vendida = productos_vendidos.aggregate(total_vendido=Sum('cantidad'))['total_vendido'] or 0           
-#             except Producto.DoesNotExist:
-#                 producto_seleccionado = None
-        
-#         if categoria:
-#             categoria_seleccionada = categoria
-#             try:
-#                 productos_categoria = Producto.objects.filter(categoria__nombre=categoria)
-#                 productos_vendidos = FacturaProducto.objects.filter(
-#                     producto__in=productos_categoria
-#                 )
-#                 if fecha_inicio and fecha_fin:
-#                     productos_vendidos = productos_vendidos.filter(
-#                         factura__fecha__range=[fecha_inicio, fecha_fin]
-#                     )
-#                     diferencia = fecha_fin - fecha_inicio
-#                     cantidad_dias = diferencia.days + 1
-#                     print(f"Canfidad de dias: {cantidad_dias}")
-#                 if dia_semana:
-#                     productos_vendidos = productos_vendidos.filter(
-#                         factura__fecha__week_day=int(dia_semana)
-#                     )
-
-#                     # Obtener la fecha más antigua y más reciente de ventas los días miércoles.
-#                     primera_fecha = productos_vendidos.earliest('factura__fecha').factura.fecha
-#                     ultima_fecha = productos_vendidos.latest('factura__fecha').factura.fecha
-#                     print(f"Primera fecha: {primera_fecha}, Ultima fecha: {ultima_fecha}")
-
-#                     # Calcular la cantidad de días de la semana (miércoles) entre la primera y la última fecha
-#                     dias_totales = (ultima_fecha - primera_fecha).days
-#                     print(f"Dias totales: {dias_totales}")
-
-#                     # Verificar si la primera y última fecha son iguales
-#                     if primera_fecha == ultima_fecha:
-#                         # Si las fechas son iguales, hay al menos un día válido.
-#                         cantidad_dias = 1 # if primera_fecha.weekday() == int(dia_semana) - 1 else 0 'TUVE QUE COMENTAR ESTE IF PORQUE ME DEVUELVE 0'
-#                     else:
-#                         # Si hay más de un día en el rango, contar los días específicos.
-#                         cantidad_dias = sum(1 for i in range(dias_totales + 1)
-#                                             if (primera_fecha + timedelta(days=i)).weekday() == int(dia_semana) - 1)
-
-#                     print(f"Cantidad de dias: {cantidad_dias}")
-
-                    
-                    
-                    
-#                 cantidad_vendida = productos_vendidos.aggregate(total_vendido=Sum('cantidad'))['total_vendido'] or 0
-#             except ProductoCategoria.DoesNotExist:
-#                 categoria_seleccionada = None
-                
-#     context = {
-#         'categorias': categorias,
-#         'producto_seleccionado': producto_seleccionado,
-#         'categoria_seleccionada': categoria_seleccionada,
-#         'productos_vendidos': productos_vendidos,
-#         'cantidad_vendida': cantidad_vendida,
-#         'cantidad_promedio': cantidad_promedio,
-#         'cantidad_minima': cantidad_minima,
-#         'cantidad_maxima': cantidad_maxima,
-#         'cantidad_no_vendida': cantidad_no_vendida,
-#     }
-    
-#     return render(request, "estadisticas/index.html", context)
