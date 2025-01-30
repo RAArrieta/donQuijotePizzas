dd4111d09855c3f610c5ef7293c507ff3b5a0824
RAArrieta <alfredoarrieta99@gmail.com>
2025-01-30 16:53:40 -0300

    static

--- donQuijoteWeb/facturas/static/facturas/js/chequear_facturas.js
+++ donQuijoteWeb/facturas/static/facturas/js/chequear_facturas.js
@@ -1,13 +1,10 @@
-document.addEventListener('DOMContentLoaded', () => {
-    document.querySelectorAll('.toggle-class').forEach(div => {
-        div.addEventListener('click', () => {
-            if (div.classList.contains('no-check')) {
-                div.classList.remove('no-check');
-                div.classList.add('check');
-            } else {
-                div.classList.remove('check');
-                div.classList.add('no-check');
-            }
+document.addEventListener("DOMContentLoaded", function () {
+    const noCheckSrc = "/static/facturas/img/no_check.png";
+    const checkSrc = "/static/facturas/img/check.png";
+
+    document.querySelectorAll(".icono-opc").forEach(img => {
+        img.addEventListener("click", function () {
+            img.src = img.src.includes("no_check.png") ? checkSrc : noCheckSrc;
         });
     });
 });
